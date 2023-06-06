cfdfebf2df53e332dc5c13e4ea16a3887ecdf00f
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:02:54 +0600

    Initial commit

---
+++ AtCoder/abc229/abc229_b/27516168.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+
+#define PI acos(-1)
+#define ll long long
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+const int M=2e5+5;
+const int N=1e6+5;
+const int mod=1e9+7;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s1,s2;
+    int i,l;
+    cin >> s1 >> s2;
+    l=min((int)s1.size(),(int)s2.size());
+    reverse(s1.begin(),s1.end());
+    reverse(s2.begin(),s2.end());
+    for(i=0;i<l;i++)
+    {
+        if((s1[i]-'0')+(s2[i]-'0') > 9)
+        {
+            cout << "Hard";
+            return 0;
+        }
+    }
+    cout << "Easy";
+    return 0;
+}
