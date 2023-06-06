d139e049ddcee23528063314b9ed4b7b21115802
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:33:06 +0600

    Initial commit

---
+++ AtCoder/abc183/abc183_a/18152570.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int N=1e5+5;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int n;
+    cin >> n;
+    cout << max(0,n);
+    return 0;
+}
