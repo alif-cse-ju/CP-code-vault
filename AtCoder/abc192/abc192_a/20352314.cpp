eae4eb26f9d4a279e5794b23014ea987c11da145
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:32:01 +0600

    Initial commit

---
+++ AtCoder/abc192/abc192_a/20352314.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int N=1e5+5;
+const int mod=1e9+7;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x;
+    cin >> x;
+    cout << (x/100+1)*100-x;
+    return 0;
+}
+//
