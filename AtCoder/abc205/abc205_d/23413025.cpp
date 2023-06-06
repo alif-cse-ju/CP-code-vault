163e9429d0ec97e9fabb687c9929a709e9fc9ef6
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:29:55 +0600

    Initial commit

---
+++ AtCoder/abc205/abc205_d/23413025.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+
+#define PI acos(-1)
+#define ll long long
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+const int M=1e5+5;
+const int N=2e5+5;
+const int mod=1e9+7;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,q;
+    ll k,l,m,r,x,ans;
+    cin >> n >> q;
+    ordered_set<ll>ost;
+    while(n--)
+    {
+        cin >> x;
+        ost.insert(x);
+    }
+    while(q--)
+    {
+        cin >> k;
+        l=1,r=2e18;
+        while(l <= r)
+        {
+            m=(l+r) >> 1ll;
+            if(m-ost.order_of_key(m+1) >= k)ans=m,r=m-1;
+            else l=m+1;
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
