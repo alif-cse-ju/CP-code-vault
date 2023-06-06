00bd07b38765522498edcbb891bf649923cca8b0
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:33:35 +0600

    Initial commit

---
+++ AtCoder/abc180/abc180_a/17478763.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int a,b,n;
+    cin >> n >> a >> b;
+    cout << n-a+b;
+    return 0;
+}
