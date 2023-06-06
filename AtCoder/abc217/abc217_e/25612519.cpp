766e624f920c64917b651604729b6b2c35a4a90f
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:03:37 +0600

    Initial commit

---
+++ AtCoder/abc217/abc217_e/25612519.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k.
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+const int M=1e5+5;
+const int N=1e6+5;
+const int mod=998244353;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int q,x;
+    deque<int>dq;
+    ordered_set<int>ost;
+    cin >> q;
+    while(q--)
+    {
+        cin >> x;
+        if(x == 1)
+        {
+            cin >> x;
+            dq.emplace_back(x);
+        }
+        else if(x == 2)
+        {
+            if(!ost.empty())
+            {
+                cout << *(ost.begin()) << '\n';
+                ost.erase(ost.begin());
+            }
+            else
+            {
+                cout << dq.front() << '\n';
+                dq.pop_front();
+            }
+        }
+        else
+        {
+            while(!dq.empty())
+            {
+                ost.insert(dq.front());
+                dq.pop_front();
+            }
+        }
+    }
+    return 0;
+}
