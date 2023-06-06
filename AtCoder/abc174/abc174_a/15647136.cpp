e78e60d3e7b2e5bee87d30778bb12d7c8aec0eb8
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:35:01 +0600

    Initial commit

---
+++ AtCoder/abc174/abc174_a/15647136.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+int main()
+{
+    FAST();
+    int n;
+    cin >> n;
+    if(n > 29)cout << "Yes";
+    else cout << "No";
+    return 0;
+}
