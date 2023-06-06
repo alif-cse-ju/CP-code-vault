6eadff74eb63fda0c36069cd44f09bc53b37f4f4
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:36:02 +0600

    Initial commit

---
+++ AtCoder/abc169/abc169_c/13883185.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define LL long long
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+int main()
+{
+    FAST();
+    LL x;
+    long double y;
+    cin >> x >> y;
+    cout << (LL)(x*y);
+    return 0;
+}
