7dcd81958d8ee921d29aabc59742a0b161d59768
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:29:19 +0600

    Initial commit

---
+++ AtCoder/abc209/abc209_c/24171744.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+const int M=1e5+5;
+const int N=2e5+5;
+const int mod=1e9+7;
+
+ll a[N];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int i,n;
+    ll ans=1;
+    cin >> n;
+    for(i=0;i<n;i++)cin >> a[i];
+    sort(a,a+n);
+    for(i=0;i<n;i++)ans=(ans*max(0ll,a[i]-i))%mod;
+    cout << ans;
+    return 0;
+}
