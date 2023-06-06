072549a7bfd9cb0e852118ca770921b900cee0fa
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 20:42:32 +0600

    Initial commit

---
+++ AtCoder/abc247/abc247_a/30963287.cpp
@@ -0,0 +1,33 @@
+#pragma GCC optimize("Ofast")
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+
+template<typename temp>using ordered_set = tree<temp, null_type, less<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+const int M=2e5+5;
+const int N=2e6+5;
+const int mod=998244353;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    char s[7];
+    cin >> s+1, s[0]='0';
+    for(int i=0;i<4;i++)cout << s[i];
+    return 0;
+}
