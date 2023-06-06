1a6ef52594c538b2202362d951c7ebce2b925c44
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:36:39 +0600

    Initial commit

---
+++ AtCoder/abc164/abc164_d/12401955.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+
+
+
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+
+
+
+
+LL big_mod(LL val,LL pow,LL mod)
+{
+    LL res = 1;
+    val = val % mod;
+    while(pow > 0)
+    {
+        if(pow & 1)
+        {
+            res = (res*val) % mod;
+        }
+        pow = pow >> 1;
+        val = (val*val) % mod;
+    }
+    return res;
+}
+
+int main()
+{
+    FAST();
+
+//    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+
+    MII mp;
+    string s;
+    int i,l,x=0,ans=0,mod=2019;
+    mp[0]=1;
+    cin >> s;
+    l=s.SZ;
+    reverse(s.B,s.E);
+    for(i=0;i<l;i++)
+    {
+        x+=(s[i]-'0')*big_mod(10,i,mod);
+        x%=mod;
+        ans+=mp[x];
+        ++mp[x];
+    }
+    cout << ans;
+    return 0;
+}
