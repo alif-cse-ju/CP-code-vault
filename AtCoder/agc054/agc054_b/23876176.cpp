0b32ad50b970c51faa42d71094319d6ba857b591
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:29:31 +0600

    Initial commit

---
+++ AtCoder/agc054/agc054_b/23876176.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k.
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+const int M=1e4+5;
+const int N=2e5+5;
+const int mod=998244353;
+
+int n,a[102];
+ll dp[102][102][M],fact[102];
+
+ll DP(int idx,int cnt,int rem)
+{
+    if(rem < 0)return 0;
+    if(idx > n)
+    {
+        if(!rem)return (fact[cnt]*fact[n-cnt])%mod;
+        return 0;
+    }
+    ll &ans=dp[idx][cnt][rem];
+    if(ans > -1)return ans;
+    ans=DP(idx+1,cnt+1,rem-a[idx])+DP(idx+1,cnt,rem);
+    return ans%=mod;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int i,sum=0;
+    fact[0]=1;
+    cin >> n;
+    for(i=1;i<=n;i++)
+    {
+        cin >> a[i];
+        sum+=a[i];
+        fact[i]=(fact[i-1]*i)%mod;
+    }
+    if(sum & 1)cout << "0";
+    else
+    {
+        memset(dp,-1,sizeof(dp));
+        cout << DP(1,0,sum/2);
+    }
+    return 0;
+}
