9000ad6a021a1a98c354d2e08c4f0e6f9ead2698
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:31:33 +0600

    Initial commit

---
+++ AtCoder/abc196/abc196_a/21148587.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int M=2e5+5;
+const int N=5e2+5;
+const int mod=1e9+7;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    cout << b-c;
+    return 0;
+}
+//
