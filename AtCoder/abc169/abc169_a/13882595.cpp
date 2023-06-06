a091ec6d32f62111c09e2a562d5f4f85f6a52322
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:36:03 +0600

    Initial commit

---
+++ AtCoder/abc169/abc169_a/13882595.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+int main()
+{
+    FAST();
+    int a,b;
+    cin >> a >> b;
+    cout << a*b;
+    return 0;
+}
