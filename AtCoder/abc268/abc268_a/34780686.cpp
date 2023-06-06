cb91e6d1f1030433b9a74d36e4ad85b79747c4a2
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 20:40:44 +0600

    Initial commit

---
+++ AtCoder/abc268/abc268_a/34780686.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a, b, c, d, e;
+  cin >> a >> b >> c >> d >> e;
+  
+  int ans = 0;
+  ans++;
+  if(b != a) ans++;
+  if(c != a && c != b) ans++;
+  if(d != a && d != b && d != c) ans++;
+  if(e != a && e != b && e != c && e != d) ans++;
+  
+  cout << ans << endl;
+  return 0;
+}
