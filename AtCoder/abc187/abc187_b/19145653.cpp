9a2ef31b93fc620c403701e18188716b9636f63b
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:32:32 +0600

    Initial commit

---
+++ AtCoder/abc187/abc187_b/19145653.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define G getline
+#define C clear()
+#define B begin()
+#define F front()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define LL long long
+#define ULL unsigned long long
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<LL,int>
+#define PLL pair<LL,LL>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<LL,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+
+
+
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+
+
+
+const int N=1e3+5;
+const int mod=1e9+7;
+
+double X[N],Y[N];
+
+int main()
+{
+    FAST();
+    int i,j,n,ans=0;
+    cin >> n;
+    for(i=1;i<=n;i++)cin >> X[i] >> Y[i];
+    for(i=2;i<=n;i++)
+    {
+        for(j=i-1;j>0;j--)
+        {
+            double dx=X[i]-X[j];
+            double dy=Y[i]-Y[j];
+            if(dy/dx >= -1.0  &&  dy/dx <= 1.0)++ans;
+        }
+    }
+    cout << ans;
+    return 0;
+}
