2b2535fa5fd0ebe18ee2b1d50e78005f7cd57e3f
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:31:16 +0600

    Initial commit

---
+++ AtCoder/abc191/abc191_a/21548633.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+const int M=1e5+5;
+const int N=2e5+5;
+const int mod=1e9+7;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int d,v,t,s;
+    cin >> v >> t >> s >> d;
+    if(d >= v*t  &&  d <= v*s)cout << "No";
+    else cout << "Yes";
+    return 0;
+}
+//
