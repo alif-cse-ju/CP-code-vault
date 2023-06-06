1742f6ec785fa26ba98390c2eb7931d4f9504675
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:02:58 +0600

    Initial commit

---
+++ AtCoder/abc228/abc228_e/27399944.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+
+#define PI acos(-1)
+#define ll long long
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+const int M=2e5+5;
+const int N=1e6+5;
+const int mod=998244353;
+
+long long BigMod(long long val,long long pow, long long mod)
+{
+    long long res = 1;
+    val = val % mod;
+    while(pow > 0)
+    {
+        if(pow & 1)res = (res*val) % mod;
+        pow = pow >> 1;
+        val = (val*val) % mod;
+    }
+    return res;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll k,m,n;
+    cin >> n >> k >> m;
+    if(m%mod == 0)cout << "0";
+    else cout << BigMod(m,BigMod(k,n,mod-1),mod);
+    return 0;
+}
