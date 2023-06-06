a2a5dc4eb61be3fa8f1b75dd4064708fbb919dbe
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:31:54 +0600

    Initial commit

---
+++ AtCoder/abc193/abc193_c/20557035.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define G getline
+#define C clear()
+#define B begin()
+#define F front()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define LL long long
+#define ULL unsigned long long
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<LL,int>
+#define PLL pair<LL,LL>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<LL,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+
+
+
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+
+
+
+const int N=1e5+5;
+const int mod=1e9+7;
+
+bool mark[N];
+
+int main()
+{
+    FAST();
+    long long i,j,n,ans,cnt;
+    cin >> n;
+    ans=n;
+    for(i=2;i*i<=n;i++)
+    {
+        if(mark[i])continue;
+        cnt=0;
+        for(j=i;j<=n;j*=i)
+        {
+            if(j < N)mark[j]=1;
+            ++cnt;
+        }
+        ans-=cnt-1;
+    }
+    cout << ans;
+    return 0;
+}
