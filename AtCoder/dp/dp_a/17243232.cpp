6bfbea280e619be1951549547fea0369c5e35fb8
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:33:56 +0600

    Initial commit

---
+++ AtCoder/dp/dp_a/17243232.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define G getline
+#define C clear()
+#define B begin()
+#define F front()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define LL long long
+#define ULL unsigned long long
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<LL,int>
+#define PLL pair<LL,LL>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<LL,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+
+
+
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+
+
+const int N=1e5+5;
+LL dp[N];
+int h[N];
+
+int main()
+{
+    FAST();
+    int i,n;
+    cin >> n;
+    for(i=1;i<=n;i++)cin >> h[i];
+    dp[2]=abs(h[1]-h[2]);
+    for(i=3;i<=n;i++)dp[i]=min(dp[i-1]+abs(h[i]-h[i-1]),dp[i-2]+abs(h[i]-h[i-2]));
+    cout << dp[n];
+    return 0;
+}
