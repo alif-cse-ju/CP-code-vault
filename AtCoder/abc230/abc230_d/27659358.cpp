11e508669e8f29f930a30db9cd187976b1749f3b
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:02:42 +0600

    Initial commit

---
+++ AtCoder/abc230/abc230_d/27659358.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+
+#define PI acos(-1)
+#define ll long long
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+const int M=2e5+5;
+const int N=2e6+5;
+const int mod=1e9+7;
+
+pair<ll,ll>a[M];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll d,l=-1e9;
+    int ans=0,i,n;
+    cin >> n >> d;
+    for(i=1;i<=n;i++)cin >> a[i].second >> a[i].first;
+    sort(a+1,a+n+1);
+    for(i=1;i<=n;i++)
+    {
+        if(a[i].second <= l+d-1)continue;
+        ++ans, l=a[i].first;
+    }
+    cout << ans;
+    return 0;
+}
