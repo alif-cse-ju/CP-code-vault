34869b7c03a3ed566bd8cea1b87ed8eb9e42dc70
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:00:13 +0600

    Initial commit

---
+++ AtCoder/abc243/abc243_a/30106191.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+
+#define PI acos(-1)
+#define ll long long
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+const int M=2e5+5;
+const int N=2e6+5;
+const int mod=1e9+7;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a,b,c,v;
+    cin >> v >> a >> b >> c;
+    v %= (a+b+c);
+    if(v < a)cout << "F";
+    else if(v < a+b)cout << "M";
+    else cout << "T";
+    return 0;
+}
