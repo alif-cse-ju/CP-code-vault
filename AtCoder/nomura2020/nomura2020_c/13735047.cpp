9a0237d7e5247373b30bb097f6c1526e92d780ad
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:36:05 +0600

    Initial commit

---
+++ AtCoder/nomura2020/nomura2020_c/13735047.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+
+
+
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+
+
+
+
+
+
+int main()
+{
+    FAST();
+
+//    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+
+    int i,n;
+    ULL mx,ans=1,a[100005],b[100005];
+    cin >> n;
+    for(i=0;i<=n;i++)cin >> a[i];
+    if(a[0] > 1  ||  (a[0] == 1  &&  n > 0))
+    {
+        cout << "-1";
+        return 0;
+    }
+    b[0]=1;
+    for(i=1;i<=n;i++)
+    {
+        mx=min(2ULL*b[i-1],(ULL)1e18);
+        if(a[i] > mx)
+        {
+            cout << "-1";
+            return 0;
+        }
+        if(i < n)b[i]=mx-a[i],ans+=mx;
+        else ans+=a[i],b[i]=0;
+    }
+    for(i=n-1;i>0;i--)
+    {
+        if(b[i] > a[i+1]+b[i+1])
+        {
+            ans-=b[i]-a[i+1]-b[i+1];
+            b[i]=a[i+1]+b[i+1];
+        }
+        else break;
+    }
+    cout << ans;
+    return 0;
+}
