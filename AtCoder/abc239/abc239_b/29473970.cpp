15f6231ef0d3e82826b5edad206e0051db421402
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 21:00:59 +0600

    Initial commit

---
+++ AtCoder/abc239/abc239_b/29473970.cpp
@@ -0,0 +1,11 @@
+    #include <iostream>
+    using namespace std;
+    int main() {
+      long long X;
+      cin >> X;
+      if (X < 0 and X % 10 != 0) {
+        cout << X / 10 - 1 << "\n";
+      } else {
+        cout << X / 10 << "\n";
+      }
+    }
