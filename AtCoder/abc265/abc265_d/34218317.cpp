610140f0a73432ec9257e5fa55c6148292c05984
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-06 20:41:03 +0600

    Initial commit

---
+++ AtCoder/abc265/abc265_d/34218317.cpp
@@ -0,0 +1,45 @@
+#pragma GCC optimize("Ofast")
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+
+#define PI acos(-1)
+#define ll long long
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+const int M=3e5+5;
+const int N=2e6+5;
+const int mod=1e9+7;
+
+ll a[M];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; ll p,q,r;cin >> n >> p >> q >> r;
+    map<ll,bool>mp; mp[0]=1;
+    for(int i=1;i<=n;i++)cin >> a[i], a[i]+=a[i-1], mp[a[i]]=1;
+    for(int i=n;i;i--)
+    {
+        if(mp.find(a[i]-p) == mp.end())continue;
+        if(mp.find(a[i]+q) == mp.end())continue;
+        if(mp.find(a[i]+q+r) == mp.end())continue;
+        cout << "Yes";
+        return 0;
+    }
+    cout << "No";
+    return 0;
+}
