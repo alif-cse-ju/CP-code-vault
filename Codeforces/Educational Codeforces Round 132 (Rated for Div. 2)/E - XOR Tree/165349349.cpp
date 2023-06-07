9ab40f5d8603d033bcee2da9a6a31b51c2f250f3
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-07 11:08:57 +0600

    Initial commit

---
+++ Codeforces/Educational Codeforces Round 132 (Rated for Div. 2)/E - XOR Tree/165349349.cpp
@@ -0,0 +1,71 @@
+#pragma GCC optimize("Ofast")
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+
+template<typename temp>using ordered_set = tree<temp, null_type, less<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+const int M=2e5+5;
+const int N=2e6+5;
+const int mod=1e9+7;
+
+set<int>st[M];
+vector<int>adj[M];
+int a[M],ans,xr[M];
+
+void DFS1(int x,int p)
+{
+    xr[x]=(xr[p]^a[x]);
+    for(int y : adj[x])
+    {
+        if(p ^ y)DFS1(y,x);
+    }
+}
+
+void DFS2(int x,int p)
+{
+    bool bad=0;
+    st[x].insert(xr[x]);
+    for(int y : adj[x])
+    {
+        if(p ^ y)
+        {
+            DFS2(y,x);
+            if((int)st[x].size() < (int)st[y].size())st[x].swap(st[y]);
+            for(int v : st[y])bad |= (st[x].find((a[x]^v)) != st[x].end());
+            for(int v : st[y])st[x].insert(v);
+            st[y].clear();
+        }
+    }
+    if(bad)++ans, st[x].clear();
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;cin >> n;
+    for(int i=1;i<=n;i++)cin >> a[i];
+    for(int i=1;i<n;i++)
+    {
+        int x,y;cin >> x >> y;
+        adj[x].emplace_back(y);
+        adj[y].emplace_back(x);
+    }
+    DFS1(1,0), DFS2(1,0);
+    cout << ans;
+    return 0;
+}
