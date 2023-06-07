2b22137fa951cdc52059d93d12d377fefa59cff9
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-07 21:09:46 +0600

    Initial commit

---
+++ Codeforces/Ozon Tech Challenge 2020 (Div.1 + Div.2/E - Kuroni and the Score Distribution/139469220.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=2e5+5;
+const int N=2e6+5;
+const int mod=1e9+7;
+int main()
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int i,j,m,mx=0,n;
+    cin >> n >> m;
+    for(i=0;i<n;i++)mx += i/2;
+    if(mx < m)cout << "-1";
+    else
+    {
+        if(mx == m)
+        {
+            for(i=1;i<=n;i++)cout << i << ' ';
+        }
+        else
+        {
+            mx=0;
+            for(i=1; ;i++)
+            {
+                mx += (i-1)/2;
+                if(mx > m)break;
+            }
+            for(j=1;j<i;j++)cout << j << ' ';
+            mx-=(i-1)/2, cout << 2*i-1-2*(m-mx) << ' ';
+            for(i=i+1;i<=n;i++)cout << 100000000+10000*i+1 << ' ';
+        }
+    }
+    return 0;
+}
