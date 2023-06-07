b92c75e0b14383796a1627b3a64da044a5cebe7b
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-07 19:21:05 +0600

    Initial commit

---
+++ Codeforces/Educational Codeforces Round 3/C - Load Balancing/195837722.cpp
@@ -0,0 +1,54 @@
+#pragma GCC optimize("Ofast")
+#include <map>
+#include <set>
+#include <cmath>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <chrono>
+#include <random>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <iomanip>
+#include <sstream>
+#include <assert.h>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+#define PI acos(-1)
+#define ll long long
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=3e5+5;
+const int N=2e6+5;
+const int mod=1e9+7;
+ int a[M];
+ int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, sum=0;cin >> n;
+    for(int i=1;i<=n;i++)cin >> a[i], sum+=a[i];
+    sort(a+1,a+n+1), reverse(a+1,a+n+1);
+    for(int i=1;i<=n;i++)
+    {
+        if(i <= sum%n)a[i]-=min(a[i], sum/n+1);
+        else a[i]-=min(a[i], sum/n);
+    }
+    int ans=0;
+    for(int i=1;i<=n;i++)ans += a[i];
+    cout << ans;
+    return 0;
+}
