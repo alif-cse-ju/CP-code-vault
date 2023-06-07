42743f48077c7652c7844d201ee148c83ea8ea98
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-07 11:10:52 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round 803 (Div. 2)/D - Fixed Point Guessing/162094040.cpp
@@ -0,0 +1,53 @@
+#pragma GCC optimize("Ofast")
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+
+#define PI acos(-1)
+#define ll long long
+
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+
+const int M=3e5+5;
+const int N=2e6+5;
+const int mod=1e9+7;
+
+int a[M];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin >> t;
+    while(t--)
+    {
+        int n;cin >> n;
+        int l=1,r=n;
+        while(l <= r)
+        {
+            int m=(l+r) >> 1;
+            cout << "? 1 " << m << endl;
+            int cnt=0;
+            for(int i=1;i<=m;i++)
+            {
+                int x;cin >> x;
+                cnt += (x <= m);
+            }
+            if(cnt & 1)r=m-1;
+            else l=m+1;
+        }
+        cout << "! " << r+1 << endl;
+    }
+    return 0;
+}
