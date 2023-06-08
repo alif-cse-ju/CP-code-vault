7e215b4832a0325e4e3e64a8759b1b4353644dca
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 13:12:18 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round 752 (Div. 2)/A - Era/133788871.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+//template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=5e2+5;
+const int N=1e6+5;
+const int mod=998244353;
+int main()
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll cnt;
+    int i,n,t,x;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        cnt=0;
+        for(i=0;i<n;i++)
+        {
+            cin >> x;
+            if(x > cnt+i)cnt+=x-cnt-i;
+        }
+        cout << cnt-1 << '\n';
+    }
+    return 0;
+}
