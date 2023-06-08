2a6cbf88ab262178c0d0ec23fca9a1db59fba083
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 16:27:15 +0600

    Initial commit

---
+++ Codeforces/Educational Codeforces Round 96 (Rated for Div. 2)/C - Numbers on Whiteboard/95252466.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int i,n,t,x,y;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        if(n == 2)
+        {
+            cout << "2\n";
+            cout << "1 2\n";
+        }
+        else
+        {
+            vector<int>v;
+            vector<pair<int,int>>ans;
+            for(i=1;i<=n;i++)v.emplace_back(i);
+            x=v[n-1],y=v[n-3];
+            v[n-3]=(x+y)/2;
+            ans.emplace_back(x,y);
+            v.pop_back();
+            while(v.size() > 1)
+            {
+                x=v.back(),v.pop_back();
+                y=v.back(),v.pop_back();
+                v.emplace_back((x+y)/2);
+                ans.emplace_back(y,x);
+            }
+            cout << v[0] << '\n';
+            for(auto it : ans)
+            {
+                cout << it.first << ' ' << it.second << '\n';
+            }
+        }
+    }
+    return 0;
+}
