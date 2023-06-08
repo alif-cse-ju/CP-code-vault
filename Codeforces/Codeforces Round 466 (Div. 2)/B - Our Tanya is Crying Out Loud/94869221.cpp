7fd20a6425ba0957f3fded98f656c403e5faf96f
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 16:27:40 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round 466 (Div. 2)/B - Our Tanya is Crying Out Loud/94869221.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    long long a,b,k,m,n,ans;
+    cin >> n >> k >> a >> b;
+    if(k == 1)cout << a*(n-1);
+    else
+    {
+        ans=0;
+        while(n > 1)
+        {
+            m=(n/k)*k;
+            if(m == 0)++m;
+            ans+=(n-m)*a;
+            n=m;
+            if(n == 1)break;
+            if(b < (n-n/k)*a)ans+=b,n/=k;
+            else
+            {
+                ans+=(n-1)*a;
+                break;
+            }
+        }
+        cout << ans;
+    }
+    return 0;
+}
