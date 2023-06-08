34800fa9403ac1610a28a9324abb7863041cc838
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 16:23:05 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round 682 (Div. 2)/B - Valerii Against Everyone/98353814.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ const int N=5e5+5;
+ int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int i,n,t,x;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        set<int>st;
+        for(i=0;i<n;i++)
+        {
+            cin >> x;
+            st.insert(x);
+        }
+        if((int)st.size() == n)cout << "NO\n";
+        else cout << "YES\n";
+    }
+    return 0;
+}
