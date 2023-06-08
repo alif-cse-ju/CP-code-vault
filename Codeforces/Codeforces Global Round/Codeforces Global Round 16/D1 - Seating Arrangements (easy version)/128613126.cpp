4a4d3b39b4c6652c2f6eeb89f71f679e8c9617c2
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 19:29:41 +0600

    Initial commit

---
+++ Codeforces/Codeforces Global Round/Codeforces Global Round 16/D1 - Seating Arrangements (easy version)/128613126.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k.
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=1e5+5;
+const int N=1e6+5;
+const int mod=998244353;
+int a[305],idx[305];
+int main()
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int i,m,n,t,ans;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> m;
+        for(i=1;i<=m;i++)cin >> a[i];
+        priority_queue<pair<int,int>>pq;
+        for(i=1;i<=m;i++)pq.emplace(-a[i],i);
+        i=0;
+        while(!pq.empty())
+        {
+            auto [x,y]=pq.top();
+            pq.pop();
+            idx[y]=++i;
+        }
+        ans=0;
+        ordered_set<int>ost;
+        for(i=1;i<=m;i++)
+        {
+            ans+=ost.order_of_key(idx[i]);
+            ost.insert(idx[i]);
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
