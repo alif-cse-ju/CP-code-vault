f121facb66d51e204e78dcc4b8c41f00a14ffc8d
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 16:24:49 +0600

    Initial commit

---
+++ Codeforces/Educational Codeforces Round 97 (Rated for Div. 2)/C - Chef Monocarp/96961483.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ const int N=205;
+int a[N],dp[N][(N << 1)];
+ int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int i,j,n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        for(i=1;i<=n;i++)cin >> a[i];
+        sort(a+1,a+n+1);
+        for(i=1;i<=n;i++)
+        {
+            for(j=0;j<=n+n;j++)
+            {
+                if(j < i)dp[i][j]=1e8;
+                else dp[i][j]=min(dp[i][j-1] , dp[i-1][j-1]+abs(a[i]-j));
+            }
+        }
+        cout << dp[n][n+n] << '\n';
+    }
+    return 0;
+}
