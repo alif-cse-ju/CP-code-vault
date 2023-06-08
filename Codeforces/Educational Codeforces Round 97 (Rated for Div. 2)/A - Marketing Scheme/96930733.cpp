0e70347443319f8ea345c8d7f967273e840cd959
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 16:25:01 +0600

    Initial commit

---
+++ Codeforces/Educational Codeforces Round 97 (Rated for Div. 2)/A - Marketing Scheme/96930733.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int l,r,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> l >> r;
+        if(l*2 <= r)cout << "NO\n";
+        else cout << "YES\n";
+    }
+    return 0;
+}
