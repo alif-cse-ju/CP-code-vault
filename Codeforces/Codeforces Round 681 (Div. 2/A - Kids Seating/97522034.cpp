51c1312083c44cc18ad26ea29c537d279bdb57b0
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 16:24:19 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round 681 (Div. 2/A - Kids Seating/97522034.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int i,n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        for(i=0;i<n;i++)cout << 4*n-2*i << ' ';
+        cout << '\n';
+    }
+    return 0;
+}
