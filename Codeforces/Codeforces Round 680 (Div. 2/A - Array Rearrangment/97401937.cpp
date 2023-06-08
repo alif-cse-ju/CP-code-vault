57e55411f1265b0769119ea08fb2a2157f198d54
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 16:24:36 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round 680 (Div. 2/A - Array Rearrangment/97401937.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    bool valid;
+    int i,n,t,x,a[55],b[55];
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> x;
+        for(i=1;i<=n;i++)cin >> a[i];
+        for(i=1;i<=n;i++)cin >> b[i];
+        valid=1;
+        for(i=1;i<=n;i++)valid &= (a[i]+b[n-i+1] <= x);
+        if(valid)cout << "Yes\n";
+        else cout << "No\n";
+    }
+    return 0;
+}
