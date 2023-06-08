34dfd66ce93cb8f72e3618990b3b9ce81ab8f023
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 17:21:26 +0600

    Initial commit

---
+++ Codeforces/Others/JCPC 2022/F - Nesr El Sieve/190113067.cpp
@@ -0,0 +1,76 @@
+#pragma GCC optimize("Ofast")
+#include <map>
+#include <set>
+#include <cmath>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <chrono>
+#include <random>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <iomanip>
+#include <sstream>
+#include <assert.h>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+#define PI acos(-1)
+#define ll long long
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=3e5+5;
+const int N=1e6+5;
+const int mod=1e9+7;
+ vector<int>PrefixFunction(string s)
+{
+    int n = (int)s.length();
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++)
+    {
+        int j = pi[i-1];
+        while (j > 0 && s[i] != s[j]) j = pi[j-1];
+        if (s[i] == s[j]) j++;
+        pi[i] = j;
+    }
+    return pi;
+}
+ int Solve()
+{
+    string a,b;cin >> a >> b;
+    vector<int>pre=PrefixFunction(a+b);
+    int la=(int)a.size(), lb=(int)b.size();
+    int l=la+lb;
+    for(int i=min(la,lb);i;i--)
+    {
+        if(la%i  ||  lb%i)continue;
+        bool ok=1;
+        for(int j=i+i;j<=l  &&  ok;j+=i)
+        {
+            if(pre[j-1] < i)ok=0;
+        }
+        if(ok)return i;
+    }
+}
+ int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("sieve.in","r",stdin);
+    int t;cin >> t;
+    while(t--)cout << Solve() << '\n';
+    return 0;
+}
+/// F,
