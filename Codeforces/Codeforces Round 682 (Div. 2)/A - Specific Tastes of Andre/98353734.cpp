fb38313e3a7e70c743adb8b988b47ae600fe67d4
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 16:23:09 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round 682 (Div. 2)/A - Specific Tastes of Andre/98353734.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ const int N=5e5+5;
+ int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        while(n--)cout << "1 ";
+        cout << '\n';
+    }
+    return 0;
+}
