a47e09e90127626a8be930c33478c0c38dee1eb8
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 16:22:53 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round 683 (Div. 2/A - Add Candies/98521699.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ const int N=2e5+5;
+ int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int i,n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        cout << n-1 << '\n';
+        for(i=2;i<=n;i++)cout << i << ' ';
+        cout << '\n';
+    }
+    return 0;
+}
