2553ec5cb7b6010ead271be32f6991e9af3f218c
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 14:44:41 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round 708 (Div. 2)/C2 - k-LCM (hard version)/110507108.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e5+5;
+const int mod=1e9+7;
+int main()
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int k,n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> k;
+        k-=3;
+        while(k--)
+        {
+            n--;
+            cout << "1 ";
+        }
+        if(n)
+        {
+            if(n%3 == 0)cout << n/3 << ' ' << n/3 << ' ' << n/3 << '\n';
+            else if(n & 1)cout << "1 " << n/2 << ' ' << n/2 << '\n';
+            else
+            {
+                if((n/2) & 1)cout << "2 " << n/2-1 << ' ' << n/2-1 << '\n';
+                else cout << n/4 << ' ' << n/4 << ' ' << n/2 << '\n';
+            }
+        }
+    }
+    return 0;
