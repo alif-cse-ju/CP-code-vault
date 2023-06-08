37dec2931bc53429f3cd3ef4d295dabc7212568e
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 18:53:35 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 784 (Div. 4)/A - Division-/154859256.cpp
@@ -0,0 +1,35 @@
+#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+#define PI acos(-1)
+#define ll long long
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=3e5+5;
+const int N=2e6+5;
+const int mod=1e9+7;
+int main()
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        if(n < 1400)cout << "Division 4";
+        else if(n < 1600)cout << "Division 3";
+        else if(n < 1900)cout << "Division 2";
+        else cout << "Division 1";
+        cout << endl;
+    }
+    return 0;
+}
