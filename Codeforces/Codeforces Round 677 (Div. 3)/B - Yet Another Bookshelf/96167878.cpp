df426bf0478c7ba1fa14037f1699f3d62d3d4054
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 16:26:13 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round 677 (Div. 3)/B - Yet Another Bookshelf/96167878.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int i,n,t,a[55],ans;
+    a[0]=1;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        deque<int>dq;
+        for(i=1;i<=n;i++)
+        {
+            cin >> a[i];
+            if(!a[i])
+            {
+                if(a[i-1])dq.emplace_back(0);
+                ++dq.back();
+            }
+        }
+        if(!a[1])dq.pop_front();
+        if(!a[n] && !dq.empty())dq.pop_back();
+        ans=0;
+        while(!dq.empty())
+        {
+            ans+=dq.back();
+            dq.pop_back();
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
