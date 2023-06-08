0cb582ccbf8279924acde7ca1ebe460cb1258c82
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-08 16:28:14 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round 510 (Div. 2)/B - Vitamins/94614816.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int i,n,x,ans;
+    string s;
+    cin >> n;
+    map<string,int>mp;
+    mp["A"]=1e6;
+    mp["B"]=1e6;
+    mp["C"]=1e6;
+    mp["AB"]=1e6;
+    mp["AC"]=1e6;
+    mp["BC"]=1e6;
+    mp["ABC"]=1e6;
+    for(i=1;i<=n;i++)
+    {
+        cin >> x >> s;
+        sort(s.begin(),s.end());
+        mp[s]=min(mp[s],x);
+    }
+    ans=mp["ABC"];
+    ans=min(ans,mp["A"]+mp["B"]+mp["C"]);
+    ans=min(ans,mp["A"]+mp["BC"]);
+    ans=min(ans,mp["B"]+mp["AC"]);
+    ans=min(ans,mp["C"]+mp["AB"]);
+    ans=min(ans,mp["AB"]+min(mp["AC"],mp["BC"]));
+    ans=min(ans,mp["AC"]+min(mp["AB"],mp["BC"]));
+    ans=min(ans,mp["BC"]+min(mp["AB"],mp["AC"]));
+    if(ans >= 1e6)cout << "-1";
+    else cout << ans;
+    return 0;
+}
