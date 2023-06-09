1e1ef9d71728a395e567dc5d38a299452db54b7e
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 19:18:04 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 637 (Div. 2) - Thanks/B - Nastya and Door/77892459.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+     //int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+      int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     int i,n,k,t,mx,ans,a[200005],cnt[200005];
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> k;
+        for(i=1;i<=n;i++)
+        {
+            cin >> a[i];
+            cnt[i]=0;
+        }
+        for(i=2;i<n;i++)
+        {
+            if(a[i] > a[i-1]  &&  a[i] > a[i+1])++cnt[i];
+            cnt[i]+=cnt[i-1];
+        }
+        cnt[n]=cnt[n-1];
+        mx=-1;
+        for(i=1;i<=n-k+1;i++)
+        {
+            if(cnt[i+k-2]-cnt[i] > mx)
+            {
+                ans=i;
+                mx=cnt[i+k-2]-cnt[i];
+            }
+        }
+        cout << mx+1 << " " << ans << "\n";
+    }
+    return 0;
+}
