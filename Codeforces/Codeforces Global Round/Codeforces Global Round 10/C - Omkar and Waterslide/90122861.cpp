56a94c88b2e8e050d6123da305b2e1a07662e0d6
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 19:03:31 +0600

    Initial commit

---
+++ Codeforces/Codeforces Global Round/Codeforces Global Round 10/C - Omkar and Waterslide/90122861.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define LL long long
+#define ULL unsigned long long
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<LL,int>
+#define PLL pair<LL,LL>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<LL,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+    //int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+    LL a[200002];
+ int main()
+{
+    FAST();
+    LL ans,x;
+    int i,n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        for(i=1;i<=n;i++)cin >> a[i];
+        x=ans=0;
+        for(i=2;i<=n;i++)
+        {
+            if(a[i] >= a[i-1])x=0;
+            else
+            {
+                x=min(x,a[i-1]-a[i]);
+                a[i]+=x;
+                if(a[i] < a[i-1])
+                {
+                    x+=a[i-1]-a[i];
+                    ans+=a[i-1]-a[i];
+                    a[i]=a[i-1];
+                }
+            }
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
