fc1b48e94111595a6ccd7dc9b4d49e8be437e4b9
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 19:09:24 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 654 (Div. 2)/A - Magical Sticks/85699341.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+int main()
+    FAST();
+    int n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        cout << (n+1)/2 << '\n';
+    }
+    return 0;
