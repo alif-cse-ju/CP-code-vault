282d0a1367e294724b161cb0e6670a83cba00990
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 21:09:06 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 579 (Div. 3)/C - Common Divisors/58891855.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+       int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     int i,n;
+    LL gcd,x;
+    cin >> n >> gcd;
+    for(i=1; i<n; i++)
+    {
+        cin >> x;
+        gcd=__gcd(gcd,x);
+    }
+    LL j,rt=sqrt(gcd);
+    x=2;
+    for(j=2; j<=rt; j++)
+    {
+        if(gcd%j == 0)
+        {
+            x+=2;
+        }
+    }
+    if(rt*rt == gcd)
+    {
+        x--;
+    }
+    cout << x;
+    return 0;
+}
