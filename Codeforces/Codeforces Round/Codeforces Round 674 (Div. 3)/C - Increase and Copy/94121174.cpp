372625cb4856892221dae5f31f06ff0fe85fd237
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 18:56:44 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 674 (Div. 3)/C - Increase and Copy/94121174.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int i,l,n,t,ans,check;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        ans=n-1;
+        l=min(n,63244);
+        for(i=2;i<=l;i++)
+        {
+            check=(n/i)+(n%i > 0)+i-2;
+            ans=min(ans,check);
+        }
+        cout << ans << '\n';
+    }
+    return 0;
