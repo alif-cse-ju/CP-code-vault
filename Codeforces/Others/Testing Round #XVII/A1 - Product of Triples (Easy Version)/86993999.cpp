2cc9a690bb2c1a67a27f14a788a0ad3cfd106e93
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 19:08:16 +0600

    Initial commit

---
+++ Codeforces/Others/Testing Round #XVII/A1 - Product of Triples (Easy Version)/86993999.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+int main()
+    FAST();
+    int i,j,k,n,x,cnt[2002]={0};
+    cin >> n;
+    for(i=1;i<14;i++)
+    {
+        for(j=i;j<=sqrt(n);j++)
+        {
+            for(k=j;k<=n;k++)
+            {
+                x=i*j*k;
+                if(x <= n)++cnt[x];
+                else break;
+            }
+        }
+    }
+    for(i=1;i<=n;i++)cout << cnt[i] << '\n';
+    return 0;
