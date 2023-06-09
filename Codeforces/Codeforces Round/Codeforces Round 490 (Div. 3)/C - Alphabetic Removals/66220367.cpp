d0485a7029025ca0779e52005ba1aba11faae9e6
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 20:49:56 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 490 (Div. 3)/C - Alphabetic Removals/66220367.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ template<typename temp>using ordered_set = tree<temp, null_type, less<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+ //int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+      int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     int i,n,k;
+    string s;
+    PQ<pair<char,int>,V<pair<char,int> >,greater<pair<char,int> > >pq1;
+    PQ<pair<int,char>,V<pair<int,char> >,greater<pair<int,char> > >pq2;
+    cin >> n >> k >> s;
+    if(k < n)
+    {
+        for(i=0;i<n;i++)pq1.PS({s[i],i});
+        while(k--)pq1.PP;
+        while(!pq1.EM)
+        {
+            pq2.PS({pq1.T.SE,pq1.T.FI});
+            pq1.PP;
+        }
+        while(!pq2.EM)cout << pq2.T.SE,pq2.PP;
+    }
+    return 0;
+}
