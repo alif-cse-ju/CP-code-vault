2677ef48fac4020336bbf8e29dfd845067a51005
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 21:47:19 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 562 (Div. 2)/B - Pairs/54708979.cpp
@@ -0,0 +1,146 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+ LL big_mod(LL val,LL pow,LL mod);
+LL mod_inverse(LL val,LL mod);
+    map<PII,int>mp;
+V<PII>v;
+V<int>mark;
+ int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     int i,j,m,n,x,y;
+    PII temp;
+    cin >> n >> m;
+    int freq[n+2];
+    memset(freq,0,sizeof(freq));
+    for(i=0; i<m; i++)
+    {
+        cin >> x >> y;
+        if(freq[x] == 0)
+        {
+            mark.PSB(x);
+        }
+        if(freq[y] == 0)
+        {
+            mark.PSB(y);
+        }
+        freq[x]++;
+        freq[y]++;
+        temp=MP(x,y);
+        if(mp[temp] == 0)
+        {
+            mp[temp]=1;
+        }
+        else
+        {
+            mp[temp]++;
+        }
+        temp=MP(y,x);
+        if(mp[temp] == 0)
+        {
+            mp[temp]=1;
+        }
+        else
+        {
+            mp[temp]++;
+        }
+    }
+    for(i=0;i<mark.SZ;i++)
+    {
+        v.PSB(MP(freq[mark[i]],mark[i]));
+    }
+    sort(v.B,v.E);
+    for(i=v.SZ-1;v[i].FI>=m/2 && i>=0;i--)
+    {
+        for(j=i-1;v[i].FI+v[j].FI>=m && j>=0;j--)
+        {
+            if(v[i].FI+v[j].FI-mp[MP(v[i].SE,v[j].SE)] == m)
+            {
+                cout << "YES";
+                return 0;
+            }
+        }
+    }
+    cout << "NO";
+    return 0;
+}
+       LL big_mod(LL val,LL pow,LL mod)
+{
+    LL res = 1;
+    val = val % mod;
+    while(pow > 0)
+    {
+        if(pow & 1)
+        {
+            res = (res*val) % mod;
+        }
+        pow = pow >> 1;
+        val = (val*val) % mod;
+    }
+    return res;
+}
+ LL mod_inverse(LL val,LL mod)
+{
+    LL mod1 = mod;
+    if(mod == 1)
+    {
+        return 0;
+    }
+    LL y = 0;
+    LL x = 1;
+    LL quotient,temp;
+    while(val > 1)
+    {
+        quotient = val / mod;
+        temp = mod;
+        mod = val % mod;
+        val = temp;
+        temp = y;
+        y = x - quotient * y;
+        x = temp;
+    }
+    if(x < 0)
+    {
+        x += mod1;
+    }
+    return x;
+}
