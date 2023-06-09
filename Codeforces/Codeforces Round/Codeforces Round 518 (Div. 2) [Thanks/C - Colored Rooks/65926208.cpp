6f5d0708a4fc2179c2477d632289cfa46bd05c34
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 20:51:36 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 518 (Div. 2) [Thanks/C - Colored Rooks/65926208.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ template<typename temp>using ordered_set = tree<temp, null_type, less<temp>, rb_tree_tag,tree_order_statistics_node_update>;
+ //int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+      int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     int i,m,n,x,y;
+    cin >> n >> m;
+    V<PII>v[n+1];
+    for(i=1;i<=n;i++)v[i].PSB({i,i});
+    for(i=1;i<=m;i++)
+    {
+        cin >> x >> y;
+        v[x].PSB({n+i,x});
+        v[y].PSB({n+i,y});
+    }
+    for(i=1;i<=n;i++)
+    {
+        cout << v[i].SZ << '\n';
+        for(auto p : v[i])cout << p.FI << ' ' << p.SE << '\n';
+    }
+    return 0;
+}
