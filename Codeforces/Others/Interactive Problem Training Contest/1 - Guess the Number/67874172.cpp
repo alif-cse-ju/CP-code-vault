e95cbc6d56607c1badc14c39cc593351907ba44e
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 19:53:06 +0600

    Initial commit

---
+++ Codeforces/Others/Interactive Problem Training Contest/1 - Guess the Number/67874172.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB emplace_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+     //int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+      int main()
+{
+//    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     int low=1,mid,high=1000000;
+    char check[5];
+    while(low < high)
+    {
+        mid=(low+high+1) >> 1;
+        P("%d\n",mid);
+        fflush(stdout);
+        S("%s",check);
+        if(strlen(check) == 1  &&  check[0] == '<')high=mid-1;
+        else low=mid;
+    }
+    P("! %d\n",low);
+    fflush(stdout);
+    return 0;
+}
