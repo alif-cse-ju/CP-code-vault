2d45929594dfed89e2dcb655cebdfa36f98953dd
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 21:02:06 +0600

    Initial commit

---
+++ Codeforces/Codeforces Global Round/Codeforces Global Round 5/B - Balanced Tunnel/62757262.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+     //int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+    int exit_time[100002];
+int enter[100002];
+ int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     int i,j,n,x;
+    cin >> n;
+    for(i=1;i<=n;i++)cin >> x,enter[x]=i;
+    for(i=1;i<=n;i++)cin >> x,exit_time[i]=enter[x];
+    int ans=0,mn=INT_MAX;
+    for(i=n;i>0;i--)
+    {
+        mn=min(mn,exit_time[i]);
+        ans+=(exit_time[i] > mn);
+    }
+    cout << ans;
+    return 0;
+}
