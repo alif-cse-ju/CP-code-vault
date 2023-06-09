4ec5a37245cf9cf434de8016dc93db3a730064cb
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 21:16:13 +0600

    Initial commit

---
+++ Codeforces/Codeforces Global Round/Codeforces Global Round 4/B - WOW Factor/57437145.cpp
@@ -0,0 +1,188 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ LL big_mod(LL val,LL pow,LL mod);
+LL mod_inverse(LL val,LL mod);
+string sum_of_two_strings(string a,string b);
+string division_of_string(string a,LL divisor);
+    V<LL>o_mark;
+ int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+      LL i,l,cnt=0;
+    string s;
+    cin >> s;
+    l=s.SZ;
+    LL l_v_pair_cnt[l+5],r_v_pair_cnt[l+5];
+    for(i=0;i<l;i++)
+    {
+        if(s[i] == 'o')
+        {
+            l_v_pair_cnt[i]=cnt;
+            o_mark.PSB(i);
+        }
+        else
+        {
+            if(i > 0  &&  s[i-1] == 'v')
+            {
+                cnt++;
+            }
+        }
+    }
+    cnt=0;
+    for(i=l-1;i>=0;i--)
+    {
+        if(s[i] == 'o')
+        {
+            r_v_pair_cnt[i]=cnt;
+        }
+        else
+        {
+            if(i > 0  &&  s[i-1] == 'v')
+            {
+                cnt++;
+            }
+        }
+    }
+    l=o_mark.SZ;
+    cnt=0;
+    for(i=0;i<l;i++)
+    {
+        cnt+=(l_v_pair_cnt[o_mark[i]] * r_v_pair_cnt[o_mark[i]]);
+    }
+    cout << cnt;
+    return 0;
+}
+        LL big_mod(LL val,LL pow,LL mod)
+{
+    LL res = 1;
+    val = val % mod;
+    while(pow > 0)
+    {
+        if(pow & 1)
+        {
+            res = (res*val) % mod;
+        }
+        pow = pow >> 1;
+        val = (val*val) % mod;
+    }
+    return res;
+}
+ LL mod_inverse(LL val,LL mod)
+{
+    LL mod1 = mod;
+    if(mod == 1)
+    {
+        return 0;
+    }
+    LL y = 0;
+    LL x = 1;
+    LL quotient,temp;
+    while(val > 1)
+    {
+        quotient = val / mod;
+        temp = mod;
+        mod = val % mod;
+        val = temp;
+        temp = y;
+        y = x - quotient * y;
+        x = temp;
+    }
+    if(x < 0)
+    {
+        x += mod1;
+    }
+    return x;
+}
+ string sum_of_two_strings(string a,string b)
+{
+    reverse(a.B,a.E);
+    reverse(b.B,b.E);
+    char c;
+    string ans="";
+    int length_of_a=a.SZ,length_of_b=b.SZ;
+    int i,carry=0,sum,length=max(length_of_a,length_of_b);
+    for(i=0; i<length; i++)
+    {
+        if(i >= length_of_a)
+        {
+            sum=(b[i]-48)+carry;
+        }
+        else if(i >= length_of_b)
+        {
+            sum=(a[i]-48)+carry;
+        }
+        else
+        {
+            sum=(a[i]-48)+(b[i]-48)+carry;
+        }
+        carry=sum/10;
+        sum%=10;
+        c=sum+48;
+        ans+=c;
+    }
+    if(carry > 0)
+    {
+        ans+=(carry+48);
+    }
+    reverse(ans.B,ans.E);
+    return ans;
+}
+ string division_of_string(string a,LL divisor)
+{
+    string ans="";
+    int i=0,l=a.SZ;
+    LL temp=a[0]-'0';
+    while(temp < divisor)
+    {
+        temp=temp*10+(a[++i]-'0');
+    }
+    while(i < l)
+    {
+        ans+=((temp/divisor)+'0');
+        temp=(temp%divisor)*10+(a[++i]-'0');
+    }
+    if(ans.SZ == 0)
+    {
+        return "0";
+    }
+    return ans;
+}
