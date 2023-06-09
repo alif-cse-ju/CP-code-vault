747a7d784ad7084e4f537d088bce8bdad73f99a0
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 21:06:30 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 515 (Div. 3)/B - Heaters/59789367.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+     //int dx[]= {-1,1,0,0};
+//int dy[]= {0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+    bool mark[1003];
+ int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("input.txt","w",stdout);
+     int i,j,n,r;
+    bool check;
+    cin >> n >> r;
+    int a[n+2];
+    for(i=1;i<=n;i++)
+    {
+        cin >> a[i];
+        if(a[i] == 1)
+        {
+            for(j=i;j>0 && j>i-r;j--)
+            {
+                mark[j]=1;
+            }
+            for(j=i+1;j<=n && j<i+r;j++)
+            {
+                mark[j]=1;
+            }
+        }
+    }
+    for(i=1;i<=n;i++)
+    {
+        if(!mark[i])
+        {
+            cout << "-1";
+            return 0;
+        }
+    }
+    int ans=0,flag=-1;
+    for(i=1;i<=n;)
+    {
+        check=false;
+        for(j=i;j<i+r  &&  j<=n;j++)
+        {
+            if(a[j])
+            {
+                flag=j;
+            }
+        }
+        ans++;
+        for(j=flag+r;j<flag+2*r  &&  j<=n;j++)
+        {
+            if(a[j])
+            {
+                check=true;
+            }
+        }
+        if(check  ||  flag+r-1 >= n)
+        {
+            i=flag+r;
+        }
+        else
+        {
+            i=flag+1;
+        }
+    }
+    cout << ans;
+    return 0;
+}
