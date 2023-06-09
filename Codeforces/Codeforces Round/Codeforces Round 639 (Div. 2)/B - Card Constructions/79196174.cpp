68fcd573a807d4751b867895742c26b8ddb407cf
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 19:17:06 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 639 (Div. 2)/B - Card Constructions/79196174.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+     //int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+    int sz;
+LL sum[25825];
+ void PreCal()
+{
+    int i;
+    for(i=1;i<=25822;i++)
+    {
+        sum[i]+=sum[i-1]+i-1+2*i;
+        if(sum[i] > 1000000000)
+        {
+            sz=i;
+            return;
+        }
+    }
+}
+ int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     PreCal();
+    int n,t,ans;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        ans=0;
+        while(n > 1)
+        {
+            auto it=upper_bound(sum,sum+25821,n)-sum-1;
+            ++ans,n-=sum[it];
+        }
+        cout << ans << "\n";
+    }
+    return 0;
+}
