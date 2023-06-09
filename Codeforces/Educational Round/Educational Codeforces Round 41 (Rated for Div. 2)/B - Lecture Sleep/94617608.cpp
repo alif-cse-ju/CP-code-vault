a716e25b658f902531c6446c669bcb97f16dd814
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 18:55:41 +0600

    Initial commit

---
+++ Codeforces/Educational Round/Educational Codeforces Round 41 (Rated for Div. 2)/B - Lecture Sleep/94617608.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e5+5;
+int a[N],b[N];
+int main()
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int i,k,n,x,ans=0;
+    cin >> n >> k;
+    for(i=1;i<=n;i++)cin >> a[i];
+    for(i=1;i<=n;i++)
+    {
+        cin >> x;
+        if(x)ans+=a[i];
+        else b[i]=a[i];
+        b[i]+=b[i-1];
+    }
+    x=0;
+    for(i=k;i<=n;i++)x=max(x,b[i]-b[i-k]);
+    cout << ans+x;
+    return 0;
