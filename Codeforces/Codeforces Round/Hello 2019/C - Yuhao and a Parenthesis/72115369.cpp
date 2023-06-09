5de5da5280adca69dc5e8451dc461f9e939ae445
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 19:25:54 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Hello 2019/C - Yuhao and a Parenthesis/72115369.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB emplace_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+     //int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+      int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     int i,n,ans=0,cnt,pre;
+    bool flag;
+    string s;
+    MII mp;
+    cin >> n;
+    for(i=0;i<n;i++)
+    {
+        cin >> s;
+        cnt=pre=0;
+        for(char c : s)
+        {
+            if(c == '(')++cnt;
+            else --cnt;
+        }
+        flag=true;
+        for(char c : s)
+        {
+            if(c == '(')++pre;
+            else --pre;
+            if(pre < min(cnt,0))
+            {
+                flag=false;
+                break;
+            }
+        }
+        if(flag)
+        {
+            if(mp[-cnt] > 0)++ans,--mp[-cnt];
+            else ++mp[cnt];
+        }
+    }
+    cout << ans;
+    return 0;
+}
