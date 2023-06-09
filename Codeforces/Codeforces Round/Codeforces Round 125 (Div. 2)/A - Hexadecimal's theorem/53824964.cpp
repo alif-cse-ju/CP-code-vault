f5dd4907938920390b890fa4fd6b7502efda73b1
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 21:50:13 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 125 (Div. 2)/A - Hexadecimal's theorem/53824964.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define Em empty()
+#define V vector
+#define Q queue
+#define PQ priority_queue
+#define ST stack
+#define Fi first
+#define Se second
+#define Pi acos(-1)
+#define PS push
+#define PP pop
+#define PSB push_back
+#define PPB pop_back
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+void FAST();
+LL big_mod(LL val,LL pow,LL mod);
+    LL ans[1000010];
+ void make_ans()
+{
+    int i=2;
+    ans[1]=ans[2]=1;
+    while(ans[i] <= 433494437)
+    {
+        i++;
+        ans[i]=ans[i-1]+ans[i-2];
+    }
+}
+ int main()
+{
+//    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     make_ans();
+    int x;
+    S("%d",&x);
+    P("%d 0 0",x);
+    return 0;
+}
+       void FAST()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ LL big_mod(LL val,LL pow,LL mod)
+{
+    LL res=1;
+    val = val % mod;
+    while(pow > 0)
+    {
+        if(pow & 1)
+        {
+            res = (res*val) % mod;
+        }
+        pow = pow >> 1;
+        val = (val*val) % mod;
+    }
+    return res;
+}
