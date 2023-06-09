4c6acee09299308b61c2bfe4b278c9fc9fab5cf2
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 21:01:09 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 134 (Div. 1)/A - Ice Skating/63121038.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+     //int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+    PII nodes[102];
+int parent[102];
+ int find_parent(int x)
+{
+    if(parent[x] == x)return x;
+    return parent[x]=find_parent(parent[x]);
+}
+ int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     int i,j,n,x,y,ans=0;
+    cin >> n;
+    for(i=1;i<=n;i++)parent[i]=i,cin >> nodes[i].FI >> nodes[i].SE;
+    for(i=1;i<n;i++)
+    {
+        for(j=i+1;j<=n;j++)
+        {
+            if(nodes[i].FI == nodes[j].FI  ||  nodes[i].SE == nodes[j].SE)
+            {
+                x=find_parent(i);
+                y=find_parent(j);
+                if(x != y)parent[y]=x;
+            }
+        }
+    }
+    for(i=1;i<n;i++)
+    {
+        for(j=i+1;j<=n;j++)
+        {
+            x=find_parent(i);
+            y=find_parent(j);
+            if(x != y)++ans,parent[y]=x;
+        }
+    }
+    cout << ans;
+    return 0;
+}
