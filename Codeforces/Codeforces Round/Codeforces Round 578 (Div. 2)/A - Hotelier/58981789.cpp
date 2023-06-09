dc6f2a987ce53abee4863363a3d85e85622cf147
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 21:08:49 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 578 (Div. 2)/A - Hotelier/58981789.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+       int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     int i,j,n;
+    string s,ans="0000000000";
+    cin >> n >> s;
+    for(i=0;i<n;i++)
+    {
+        if(s[i] == 'L')
+        {
+            for(j=0;j<10;j++)
+            {
+                if(ans[j] == '0')
+                {
+                    ans[j]='1';
+                    break;
+                }
+            }
+        }
+        else if(s[i] == 'R')
+        {
+            for(j=9;j>-1;j--)
+            {
+                if(ans[j] == '0')
+                {
+                    ans[j]='1';
+                    break;
+                }
+            }
+        }
+        else
+        {
+            ans[s[i]-'0']='0';
+        }
+    }
+    cout << ans;
+    return 0;
+}
