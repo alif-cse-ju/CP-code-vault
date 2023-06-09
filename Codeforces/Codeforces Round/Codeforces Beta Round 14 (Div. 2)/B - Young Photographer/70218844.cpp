b6dad00b385bf648c7a5ac638e3d06117aaadb61
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 19:30:52 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Beta Round 14 (Div. 2)/B - Young Photographer/70218844.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB emplace_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+     //int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+    int cnt[1002];
+ int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     int i,j,n,s,x,y;
+    cin >> n >> s;
+    for(i=0;i<n;i++)
+    {
+        cin >> x >> y;
+        if(x > y)swap(x,y);
+        for(j=x;j<=y;j++)++cnt[j];
+    }
+    int ans=INT_MAX;
+    for(i=0;i<1001;i++)
+    {
+        if(cnt[i] == n)ans=min(ans,abs(s-i));
+    }
+    if(ans < INT_MAX)cout << ans;
+    else cout << "-1";
+    return 0;
+}
