084c867348735080738197a6e509d6ac7e1a574e
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 18:53:23 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 678 (Div. 2)/B - Prime Square/96626224.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int ans[102][102];
+int main()
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int i,j,n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                ans[i][j]=0;
+                if(i == j)ans[i][j]=1;
+                else if(j == i+1)ans[i][j]=1;
+                else if(i == n)ans[i][1]=1;
+            }
+        }
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)cout << ans[i][j] << ' ';
+            cout << '\n';
+        }
+    }
+    return 0;
