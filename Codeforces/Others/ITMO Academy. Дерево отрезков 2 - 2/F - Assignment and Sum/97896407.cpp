8dabf01953ffd1dbeb240f1941e2df94e1f211a2
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 18:51:05 +0600

    Initial commit

---
+++ Codeforces/Others/ITMO Academy. Дерево отрезков 2 - 2/F - Assignment and Sum/97896407.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e5+5;
+long long tree[4*N],lazy[4*N];
+void Update(int nodeNum,int start,int end,int l,int r,long long value)
+    if(lazy[nodeNum] != -1)
+    {
+        tree[nodeNum]=(end-start+1)*lazy[nodeNum];
+        if(start != end)
+        {
+            lazy[nodeNum*2]=lazy[nodeNum];
+            lazy[nodeNum*2 + 1]=lazy[nodeNum];
+        }
+        lazy[nodeNum]=-1;
+    }
+    if(start > end  ||  start > r  ||  end < l)return;
+    if(start >= l  &&  end <= r)
+    {
+        tree[nodeNum]=(end-start+1)*value;
+        if(start != end)
+        {
+            lazy[nodeNum*2]=value;
+            lazy[nodeNum*2 + 1]=value;
+        }
+        return;
+    }
+    int mid=(start+end)/2;
+    Update(nodeNum*2,start,mid,l,r,value);
+    Update(nodeNum*2 + 1,mid+1,end,l,r,value);
+    tree[nodeNum]=tree[nodeNum*2] + tree[nodeNum*2 + 1];
+long long Query(int nodeNum,int start,int end,int l,int r)
+{
+    if(start > end  ||  start > r  ||  end < l)return 0;
+    if(lazy[nodeNum] != -1)
+    {
+        tree[nodeNum]=(end-start+1)*lazy[nodeNum];
+        if(start != end)
+        {
+            lazy[nodeNum*2]=lazy[nodeNum];
+            lazy[nodeNum*2 + 1]=lazy[nodeNum];
+        }
+        lazy[nodeNum]=-1;
+    }
+    if(start >= l  &&  end <= r)return tree[nodeNum];
+     int mid=(start+end)/2;
+    long long ans1=Query(nodeNum*2,start,mid,l,r);
+    long long ans2=Query(nodeNum*2 + 1,mid+1,end,l,r);
+     return (ans1+ans2);
+}
+ int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int l,m,n,r,x,op;
+    cin >> n >> m;
+    memset(lazy,-1,sizeof(lazy));
+    while(m--)
+    {
+        cin >> op;
+        if(op == 1)
+        {
+            cin >> l >> r >> x;
+            Update(1,1,n,++l,r,x);
+        }
+        else
+        {
+            cin >> l >> r;
+            cout << Query(1,1,n,++l,r) << '\n';
+        }
+    }
+    return 0;
+}
