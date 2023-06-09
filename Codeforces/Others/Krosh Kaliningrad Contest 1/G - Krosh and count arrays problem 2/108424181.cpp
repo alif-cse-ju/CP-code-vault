bff578cfedf4812886996967c4ace950991d251b
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 18:27:39 +0600

    Initial commit

---
+++ Codeforces/Others/Krosh Kaliningrad Contest 1/G - Krosh and count arrays problem 2/108424181.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=3e5+5;
+const int mod=1e9+7;
+int main()
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int i,n;
+    cin >> n;
+    for(i=1;i<n;i++)cout << '3';
+    cout << '4';
+    return 0;
