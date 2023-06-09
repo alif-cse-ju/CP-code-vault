d372a5b55cb2b3db43bdd1dad7c463770dd0b284
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 19:08:50 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 655 (Div. 2)/A - Omkar and Completion/86592685.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+int main()
+    FAST();
+    int i,n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        for(i=1;i<=n;i++)cout << "1 ";
+        cout << '\n';
+    }
+    return 0;
