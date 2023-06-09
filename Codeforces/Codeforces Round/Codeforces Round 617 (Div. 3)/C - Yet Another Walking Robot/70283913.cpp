fbcef0a85013530632362b253e6d439259ba6d86
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 19:30:35 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 617 (Div. 3)/C - Yet Another Walking Robot/70283913.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+     //int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+      int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     int i,n,t,x,y,z,x1,y1,ans;
+    string s;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> s;
+        map<PII,int>mp;
+        mp.C;
+        mp[MP(0,0)]=1;
+        x=y=0;
+        ans=INT_MAX;
+        for(i=1;i<=n;i++)
+        {
+            if(s[i-1] == 'L')x-=1;
+            else if(s[i-1] == 'R')x+=1;
+            else if(s[i-1] == 'U')y+=1;
+            else y-=1;
+            if(mp.find(MP(x,y)) == mp.E)mp[MP(x,y)]=i;
+            else
+            {
+                z=mp[MP(x,y)];
+                if(i-z < ans)
+                {
+                    ans=i-z;
+                    if(x == 0  &&  y == 0  &&  z == 1)x1=1;
+                    else x1=z+1;
+                    y1=i;
+                }
+                mp[MP(x,y)]=i;
+            }
+        }
+        if(ans < INT_MAX)cout << x1 << ' ' << y1 << '\n';
+        else cout << "-1\n";
+    }
+    return 0;
+}
