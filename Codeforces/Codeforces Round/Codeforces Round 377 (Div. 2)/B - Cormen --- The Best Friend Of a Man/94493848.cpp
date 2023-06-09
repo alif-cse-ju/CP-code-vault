91adf9e7299b56950559d72217a7a808c0451c84
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 18:55:58 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 377 (Div. 2)/B - Cormen --- The Best Friend Of a Man/94493848.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int i,k,n,x,ans=0,a[505];
+    cin >> n >> k;
+    for(i=1;i<=n;i++)cin >> a[i];
+    for(i=2;i<=n;i++)
+    {
+        x=max(0,k-a[i]-a[i-1]);
+        ans+=x,a[i]+=x;
+    }
+    cout << ans << '\n';
+    for(i=1;i<=n;i++)cout << a[i] << ' ';
+    return 0;
