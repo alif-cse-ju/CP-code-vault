a4017824ceaffdf1de65db47cdb5b2c68e86a662
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 19:36:24 +0600

    Initial commit

---
+++ Codeforces/Others/VK Cup 2016 - Round 1 (Div. 2 Edition)/A - Bear and Reverse Radewoosh/68202433.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB emplace_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+     //int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+      int main()
+{
+    FAST();
+ //    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+     int i,n;
+    LL c,ans1=0LL,ans2=0LL,x=0LL;
+    cin >> n >> c;
+    LL a[n+2],b[n+2];
+    for(i=1;i<=n;i++)cin >> a[i];
+    for(i=1;i<=n;i++)cin >> b[i];
+    for(i=1;i<=n;i++)
+    {
+        x+=b[i];
+        ans1+=max(0LL,a[i]-c*x);
+    }
+    x=0LL;
+    for(i=n;i>0;i--)
+    {
+        x+=b[i];
+        ans2+=max(0LL,a[i]-c*x);
+    }
+    if(ans1 > ans2)cout << "Limak";
+    else if(ans1 < ans2)cout << "Radewoosh";
+    else cout << "Tie";
+    return 0;
+}
