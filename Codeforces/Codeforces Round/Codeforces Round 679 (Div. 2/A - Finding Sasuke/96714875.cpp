46f742ace4037235fdcebc7e1fce9141bd1b4ce3
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-09 18:53:14 +0600

    Initial commit

---
+++ Codeforces/Codeforces Round/Codeforces Round 679 (Div. 2/A - Finding Sasuke/96714875.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int i,n,t,a[102];
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        for(i=1;i<=n;i++)cin >> a[i];
+        for(i=1;i<=n;i++)
+        {
+            if(i & 1)
+            {
+                if(a[i] > 0)cout << abs(a[i+1]) << ' ';
+                else cout << -abs(a[i+1]) << ' ';
+            }
+            else
+            {
+                if(a[i] > 0)cout << -abs(a[i-1]) << ' ';
+                else cout << abs(a[i-1]) << ' ';
+            }
+        }
+        cout << '\n';
+    }
+    return 0;
