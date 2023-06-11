20a45f533fdafae387f047f559096afcaa5cfb6d
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 16:57:27 +0600

    Initial commit

---
+++ CodeChef/SPRT2020/PROBRGN - PRO BARGAINER/40355900.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e6+5;
+const int mod=1e9+7;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        cout << n/2 << '\n';
+    }
+    return 0;
+}
