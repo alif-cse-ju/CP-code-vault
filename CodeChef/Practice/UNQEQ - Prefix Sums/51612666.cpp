e9f5c91316adc48f829e60493f0d3de3295a815d
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:43:43 +0600

    Initial commit

---
+++ CodeChef/Practice/UNQEQ - Prefix Sums/51612666.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+#define PI acos(-1)
+#define ll long long
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=1e5+5;
+const int N=1e6+5;
+const int mod=1e9+7;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int i,n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        if((n/2) & 1)cout << "NO\n";
+        else
+        {
+            cout << "YES\n";
+            vector<int>a,b;
+            for(i=1;i<=n/2;i++)
+            {
+                if(i & 1)a.emplace_back(i);
+                else b.emplace_back(i);
+            }
+            for(i=n/2+1;i<=n;i++)
+            {
+                if(i & 1)b.emplace_back(i);
+                else a.emplace_back(i);
+            }
+            for(int it : a)cout << it << ' ';
+            cout << '\n';
+            for(int it : b)cout << it << ' ';
+            cout << '\n';
+        }
+    }
+    return 0;
+}
