092bf216dc786f14b041a4f3f14550edd8b44cc7
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:57:15 +0600

    Initial commit

---
+++ CodeChef/Practice/JOKRBTMN - Joker and Batman/48978815.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key(k)  : Number of items strictly smaller than k.
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=1e5+5;
+const int N=2e5+5;
+const int mod=1e9+7;
+int a[M],clr[M];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int ans,i,k,l,m,n,t,x,y;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> m >> l;
+        for(i=1;i<=m;i++)
+        {
+            cin >> k;
+            while(k--)cin >> x,clr[x]=i;
+        }
+        ans=1;
+        cin >> y;
+        for(i=2;i<=l;i++)
+        {
+            cin >> x;
+            if(clr[x] == clr[y])continue;
+            ++ans,y=x;
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
