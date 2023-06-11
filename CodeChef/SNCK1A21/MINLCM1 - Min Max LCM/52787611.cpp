01c75ecaf9b87094ca287034128e9b82a46db8d0
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:38:55 +0600

    Initial commit

---
+++ CodeChef/SNCK1A21/MINLCM1 - Min Max LCM/52787611.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=2e5+5;
+const int N=2e6+5;
+const int mod=1e9+7;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    ll k,x;
+    cin >> t;
+    while(t--)
+    {
+        cin >> x >> k;
+        cout << 2*x << ' ' << (x*k)*(x*k-1) << '\n';
+    }
+    return 0;
+}
