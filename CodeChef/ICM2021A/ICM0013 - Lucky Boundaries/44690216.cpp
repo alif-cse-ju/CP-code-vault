ea18e1d2746661c3ce07efc1a16ddb23ba50ac48
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 16:12:09 +0600

    Initial commit

---
+++ CodeChef/ICM2021A/ICM0013 - Lucky Boundaries/44690216.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define PI acos(-1)
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=1e5+5;
+const int N=2e5+5;
+const int mod=1e9+7;
+int main()
+{
+    FAST();
+    int n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        cout << n << '\n';
+    }
+    return 0;
+}
