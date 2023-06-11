2bc6a4e2e5568e98f268583a4f39ae793dbcc83f
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 16:19:26 +0600

    Initial commit

---
+++ CodeChef/PTRN2021/ITGUY51 - P-E/41251379.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e5+5;
+const int mod=1e9+7;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int i,j,n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)cout << ((i-1)*n+j)*2-1;
+            cout << '\n';
+        }
+    }
+    return 0;
+}
+//1422
