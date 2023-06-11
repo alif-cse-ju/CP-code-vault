18b76a388d7269e4328e36766c679af9e99b7d4a
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:55:02 +0600

    Initial commit

---
+++ CodeChef/Practice/BCMOFG - Orthodox Finger/49668056.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int getMinOperations(int *arr, int n)
+{
+unordered_map<int, int> hash;
+for (int i = 0;i < n; ++i) {
+hash[arr[i]]++;
+}
+int maxFrequency = 0;
+for (auto elem : hash) {
+if (elem.second > maxFrequency) {
+maxFrequency = elem.second;
+}
+}
+return (n - maxFrequency);
+}
+int main()
+{
+int n , arr[100];
+cin>>n;
+for(int i=0;i<n;i++){
+cin>>arr[i];
+}
+cout<<getMinOperations(arr, n) << endl;
+return 0;
+}
