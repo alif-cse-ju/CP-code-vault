b8f42cd0fc73ba5da1e1dbbf182467291530dc68
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:27:20 +0600

    Initial commit

---
+++ CodeChef/START25A/TOWERTOP - Building Towers/58050364.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+#define PI acos(-1)
+#define ll long long
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=2e5+5;
+const int N=2e6+5;
+const int mod=1e9+7;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    ll ans,m,x,y;
+    cin >> t;
+    while(t--)
+    {
+        cin >> x >> m;
+        ans=y=1;
+        while(y < x)++ans, y *= 2;
+        if(ans > m)cout << "0\n";
+        else cout << m-ans+1 << '\n';
+    }
+    return 0;
+}
