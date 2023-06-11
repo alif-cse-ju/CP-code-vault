49556a200952757afff7ab6b9cbd83758bc1e8cd
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 17:04:20 +0600

    Initial commit

---
+++ CodeChef/TAWY2020/TAWY001 - The Largest Palindrome/38809771.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define G getline
+#define C clear()
+#define B begin()
+#define F front()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define LL long long
+#define ULL unsigned long long
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<LL,int>
+#define PLL pair<LL,LL>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<LL,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+int main()
+{
+    FAST();
+    int i,l;
+    LL n,x;
+    string s;
+    cin >> n;
+    x=n;
+    while(x > 0)s+='0'+x%10,x/=10;
+    reverse(s.begin(),s.end());
+    l=s.size();
+    for(i=0;i<l/2;i++)
+    {
+        if(s[i] != s[l-i-1])s[l-i-1]=s[i];
+    }
+    x=0;
+    for(char c : s)x=x*10+(c-'0');
+    if(x <= n)
+    {
+        cout << x << ' ' << n-x;
+        return 0;
+    }
+    for(i=l/2;i<l;i++)
+    {
+        if(s[i] > '0')
+        {
+            --s[i];
+            if(i != l-i-1)--s[l-i-1];
+            break;
+        }
+    }
+    x=0;
+    for(char c : s)x=x*10+(c-'0');
+    if(x > 0  &&  x <= n)
+    {
+        cout << x << ' ' << n-x;
+        return 0;
+    }
+    for(i=1;i<l;i++)cout << '9';
+    cout << " 1";
+    return 0;
+}
