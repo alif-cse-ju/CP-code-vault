f3cdad24e69c59becdf840d562a8de37c7294514
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 17:33:18 +0600

    Initial commit

---
+++ CodeChef/LICO2020/ISHGAME - Ish and String Game/34645398.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define LL long long
+#define ULL unsigned long long
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+int main()
+{
+    FAST();
+    int t;
+    string a,b;
+    bool mark[30];
+    cin >> t;
+    while(t--)
+    {
+        cin >> a >> b;
+        memset(mark,0,sizeof(mark));
+        for(char c : b)mark[c-'A']=1;
+        for(char c : a)
+        {
+            if(!mark[c-'A'])cout << c;
+        }
+        cout << '\n';
+    }
+    return 0;
+}
