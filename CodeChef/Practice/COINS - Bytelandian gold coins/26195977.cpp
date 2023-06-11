1ea63709d18c2f6fbf42ad6697d40095b20c8810
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 17:49:06 +0600

    Initial commit

---
+++ CodeChef/Practice/COINS - Bytelandian gold coins/26195977.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+map<LL,LL>dp;
+LL find_ans(LL n)
+{
+    if(n < 7)
+    {
+        return n;
+    }
+    if(dp.find(n) == dp.end())
+    {
+        return dp[n]=max(n,find_ans(n/2)+find_ans(n/3)+find_ans(n/4));
+    }
+    return dp[n];
+}
+int main()
+{
+//    FAST();
+//    freopen("input.txt","r",stdin);
+//    freopen("input.txt","w",stdout);
+    LL n;
+    while(S("%lld",&n) == 1)
+    {
+        P("%lld\n",max(n,find_ans(n/2)+find_ans(n/3)+find_ans(n/4)));
+    }
+    return 0;
+}
