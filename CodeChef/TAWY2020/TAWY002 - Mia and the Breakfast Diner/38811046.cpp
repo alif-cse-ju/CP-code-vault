8ee3f232d1a3c0fee219c11793796e4438a9f4e9
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 17:04:07 +0600

    Initial commit

---
+++ CodeChef/TAWY2020/TAWY002 - Mia and the Breakfast Diner/38811046.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define G getline
+#define C clear()
+#define B begin()
+#define F front()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define LL long long
+#define ULL unsigned long long
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<LL,int>
+#define PLL pair<LL,LL>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<LL,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+int main()
+{
+    FAST();
+    double Y,y,total=0;
+    int i,D,X,x;
+    cin >> D >> X >> Y;
+    while(D--)
+    {
+        cin >> x;
+        total+=X;
+        y=Y;
+        while(--x)y-=(y/100.0)*2.0;
+        total+=y;
+    }
+    if(total < 300.0)cout << "NO";
+    else cout << "YES";
+    return 0;
+}
