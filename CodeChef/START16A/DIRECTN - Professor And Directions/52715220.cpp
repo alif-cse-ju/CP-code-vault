6c71ca9638131584e1b21c8dba091ef6371cb318
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:39:56 +0600

    Initial commit

---
+++ CodeChef/START16A/DIRECTN - Professor And Directions/52715220.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=2e5+5;
+const int N=1e6+5;
+const int mod=1e9+7;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    bool ok;
+    string s;
+    int i,l,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> l >> s;
+        ok=0;
+        for(i=1;i<l;i++)ok |= (s[i] == s[i-1]);
+        if(ok)cout << "YES\n";
+        else cout << "NO\n";
+    }
+    return 0;
+}
