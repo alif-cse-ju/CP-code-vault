0bb38a2d969a4af58aee6c1a2113d48b3fcb160c
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:26:16 +0600

    Initial commit

---
+++ CodeChef/Practice/PERMEX - PerMEXuation/58856496.cpp
@@ -0,0 +1,43 @@
+#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+template<typename temp>using ordered_set = tree<temp, null_type, less<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=3e5+5;
+const int N=2e6+5;
+const int mod=1e9+7;
+int ans[M];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    int i,n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> s;
+        if(s[0] == '0'  ||  s[1] == '0'  ||  s[n] == '0')cout << "No\n";
+        else
+        {
+            for(i=0;i<n;i++)ans[i]=i;
+            for(i=1;i<n;i++)
+            {
+                if(s[i] == '0')swap(ans[i-1], ans[i]);
+            }
+            cout << "Yes\n";
+            for(i=0;i<n;i++)cout << ans[i] << ' ';
+            cout << '\n';
+        }
+    }
+    return 0;
+}
