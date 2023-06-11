97131c2afac21f4d50a1d77e2be2cc7bd4be00f9
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:55:36 +0600

    Initial commit

---
+++ CodeChef/AUG21A/PROBDIFF - Problem Difficulties/49493558.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a[4],ans,i,t;
+    cin >> t;
+    while(t--)
+    {
+        for(i=0;i<4;i++)cin >> a[i];
+        ans=max((a[0] != a[1]) + (a[2] != a[3]),(a[0] != a[2]) + (a[1] != a[3]));
+        ans=max(ans,(a[0] != a[3]) + (a[1] != a[2]));
+        cout << ans << '\n';
+    }
+    return 0;
+}
