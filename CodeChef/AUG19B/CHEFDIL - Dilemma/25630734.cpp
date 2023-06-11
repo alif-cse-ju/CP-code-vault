fb7ecd9614d684c4a4067ee67ea5d45b19a1d0c1
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 17:49:20 +0600

    Initial commit

---
+++ CodeChef/AUG19B/CHEFDIL - Dilemma/25630734.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+int main()
+{
+    FAST();
+//    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+    int i,t,l,cnt;
+    string s;
+    cin >> t;
+    while(t--)
+    {
+        cin >> s;
+        cnt=0;
+        l=s.SZ;
+        for(i=0; i<l; i++)
+        {
+            if(s[i] == '1')
+            {
+                cnt++;
+            }
+        }
+        if(cnt & 1)
+        {
+            cout << "WIN\n";
+        }
+        else
+        {
+            cout << "LOSE\n";
+        }
+    }
+    return 0;
+}
