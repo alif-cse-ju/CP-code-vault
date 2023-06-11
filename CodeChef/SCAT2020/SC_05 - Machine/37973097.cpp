5eaf6c0edbc39362b95c6e1e4bc5758beb062f42
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 17:06:47 +0600

    Initial commit

---
+++ CodeChef/SCAT2020/SC_05 - Machine/37973097.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define LL long long
+#define ULL unsigned long long
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<LL,int>
+#define PLL pair<LL,LL>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<LL,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+int main()
+{
+    FAST();
+    string a,b,s;
+    int i,l,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> s;
+        l=s.size();
+        a=b="";
+        for(i=0;i<l;i++)
+        {
+            if(s[i] == '7')a+='4',b+='3';
+            else a+=s[i],b+='0';
+        }
+        reverse(b.begin(),b.end());
+        while(!b.empty()  &&  b.back() == '0')b.pop_back();
+        if(b.empty())b+='0';
+        reverse(b.begin(),b.end());
+        cout << a << ' ' << b << '\n';
+    }
+    return 0;
+}
