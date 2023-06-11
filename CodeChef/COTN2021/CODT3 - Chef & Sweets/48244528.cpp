61ba58adc51441b80cdf5362fa89a8760adb850f
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:59:43 +0600

    Initial commit

---
+++ CodeChef/COTN2021/CODT3 - Chef & Sweets/48244528.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key(k)  : Number of items strictly smaller than k.
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=1e5+5;
+const int N=2e5+5;
+const int mod=1e9+7;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,s,x,y,ans=-1;
+    cin >> n >> s;
+    while(n--)
+    {
+        cin >> x >> y;
+        if(x*100+y <= s*100)
+        {
+             if(x < s  &&  y)ans=max(ans,100-y);
+             else ans=max(ans,0);
+        }
+    }
+    cout << ans;
+    return 0;
+}
+//বালের
