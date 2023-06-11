dc0af29ed80bc4c0249d16dceaac3ed964e3796a
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:19:51 +0600

    Initial commit

---
+++ CodeChef/START55A/NZXOR - Non Zero Xor/73298751.cpp
@@ -0,0 +1,39 @@
+#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+#define PI acos(-1)
+#define ll long long
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=3e5+5;
+const int N=2e6+5;
+const int mod=998244353;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin >> t;
+    while(t--)
+    {
+        int n;cin >> n;
+        int ans=0, xr=0;
+        map<int,bool>mp; mp[0]=1;
+        while(n--)
+        {
+            int x;cin >> x;
+            xr ^= x;
+            if(mp.find(xr) != mp.end())++ans, mp.clear(), xr=0;
+            mp[xr]=1;
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
