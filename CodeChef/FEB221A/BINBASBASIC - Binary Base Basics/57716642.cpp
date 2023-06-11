971f3015f5d50f70a83cf152ea0c4f3e19145e0b
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:27:48 +0600

    Initial commit

---
+++ CodeChef/FEB221A/BINBASBASIC - Binary Base Basics/57716642.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+template<typename temp>using ordered_set = tree<temp, null_type, less<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=2e5+5;
+const int N=2e6+5;
+const int mod=1e9+7;
+char s[1005];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int cnt,i,k,l,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> l >> k >> s+1;
+        for(i=1, cnt=0;i<=l/2;i++)cnt += (s[i] != s[l-i+1]);
+        if(cnt <= k)
+        {
+            if(l & 1)cout << "YES\n";
+            else if((k-cnt)%2 == 0)cout << "YES\n";
+            else cout << "NO\n";
+        }
+        else cout << "NO\n";
+    }
+    return 0;
+}
