8b7f63bd67310aba115f544be2c0dd08d5101112
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:41:39 +0600

    Initial commit

---
+++ CodeChef/CANT2021/CHEFRAT - Chef Rating/52094058.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+#define PI acos(-1)
+#define ll long long
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=2e5+5;
+const int N=1e6+5;
+const int mod=1e9+7;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    ll r,x;
+    cin >> r >> n;
+    while(n--)cin >> x,r += x;
+    cout << r;
+    return 0;
+}
