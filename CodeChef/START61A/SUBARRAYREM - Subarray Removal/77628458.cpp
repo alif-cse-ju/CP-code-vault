6d9a27aaed2d5cf6d34331021f63634a86b40c20
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:18:04 +0600

    Initial commit

---
+++ CodeChef/START61A/SUBARRAYREM - Subarray Removal/77628458.cpp
@@ -0,0 +1,56 @@
+#pragma GCC optimize("Ofast")
+#include <map>
+#include <set>
+#include <cmath>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <chrono>
+#include <random>
+#include <vector>
+#include <cstring>
+#include <iomanip>
+#include <sstream>
+#include <assert.h>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+template<typename temp>using ordered_set = tree<temp, null_type, less<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=3e5+5;
+const int N=1e6+5;
+const int mod=998244353;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin >> t;
+    while(t--)
+    {
+        int n;cin >> n;
+        int cnt0=0, cnt1=0;
+        while(n--)
+        {
+            int x;cin >> x;
+            if(x)++cnt1;
+            else ++cnt0;
+        }
+        int ans=min(cnt0, cnt1);
+        cnt1 -= ans, ans += cnt1/3;
+        cout << ans << '\n';
+    }
+    return 0;
+}
+//1268
