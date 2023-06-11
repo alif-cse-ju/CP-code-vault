a732fdd3861a67df304aec2d8d204fe53d5b8cdd
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 16:21:23 +0600

    Initial commit

---
+++ CodeChef/LTIME91A/SWAP10HG - One Zero Swaps/40775236.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define G getline
+#define C clear()
+#define B begin()
+#define F front()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define LL long long
+#define ULL unsigned long long
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<LL,int>
+#define PLL pair<LL,LL>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<LL,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+int main()
+{
+    FAST();
+    bool valid;
+    int i,l,t;
+    string a,b;
+    cin >> t;
+    while(t--)
+    {
+        cin >> l >> a >> b;
+        deque<int>one,zero;
+        for(i=0;i<l;i++)
+        {
+            if(a[i] != b[i])
+            {
+                if(a[i] == '1')one.emplace_back(i);
+                else zero.emplace_back(i);
+            }
+        }
+        if((int)one.size() != (int)zero.size())cout << "No\n";
+        else
+        {
+            valid=1;
+            l=(int)one.size();
+            while(l--)
+            {
+                if(one.front() > zero.front())
+                {
+                    valid=0;
+                    break;
+                }
+                one.pop_front();
+                zero.pop_front();
+            }
+            if(valid)cout << "Yes\n";
+            else cout << "No\n";
+        }
+    }
+    return 0;
+}
