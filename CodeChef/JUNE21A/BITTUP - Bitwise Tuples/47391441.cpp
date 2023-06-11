b12c47c17a2d7f3d2da0415efc1d8ea841002cdb
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 16:04:32 +0600

    Initial commit

---
+++ CodeChef/JUNE21A/BITTUP - Bitwise Tuples/47391441.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+#define PI acos(-1)
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=1e5+5;
+const int N=2e5+5;
+const int mod=1e9+7;
+long long BigMod(long long val,long long pow)
+{
+    long long res = 1;
+    val = val % mod;
+    while(pow > 0)
+    {
+        if(pow & 1)res = (res*val) % mod;
+        pow = pow >> 1;
+        val = (val*val) % mod;
+    }
+    return res;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int m,n,t;
+    long long ans;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> m;
+        ans=BigMod(2,n);
+        ans=(ans-1+mod)%mod;
+        ans=BigMod(ans,m);
+        cout << ans << '\n';
+    }
+    return 0;
+}
