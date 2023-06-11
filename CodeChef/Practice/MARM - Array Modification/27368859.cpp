633cbb656271cdf7c1fc73d115f7adc49f41d350
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 17:47:20 +0600

    Initial commit

---
+++ CodeChef/Practice/MARM - Array Modification/27368859.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define S scanf
+#define P printf
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define MP make_pair
+#define LL long long int
+#define ULL unsigned long long int
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<long long int,int>
+#define PLL pair<long long int,long long int>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<long long int,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+int main()
+{
+    FAST();
+//    freopen("input.txt","r",stdin);
+//    freopen("input.txt","w",stdout);
+    LL i,n,k,t,check,mid,a[10002],ans[10002];
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> k;
+        for(i=1;i<=n;i++)cin >> a[i],ans[i]=a[i];
+        mid=n/2;
+        for(i=1;i<=n  &&  i<=k;i++)
+        {
+            check=k/n+(k%n >= i);
+            if(i <= mid)
+            {
+                if(check%3 == 1)ans[i]=a[i]^a[n-i+1];
+                else if(check%3 == 2)ans[i]=a[n-i+1];
+            }
+            else if(n & 1  &&  i == mid+1)ans[i]=0;
+            else
+            {
+                if(check%3 == 2)ans[i]=a[i]^a[n-i+1];
+                else if(check%3 == 1)ans[i]=a[n-i+1];
+            }
+        }
+        cout << ans[1];
+        for(i=2;i<=n;i++)cout << ' ' << ans[i];
+        cout << '\n';
+    }
+    return 0;
+}
