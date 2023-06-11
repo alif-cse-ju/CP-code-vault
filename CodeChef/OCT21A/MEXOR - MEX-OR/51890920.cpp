dbe1d203b2f679807244ad1318630bfec0d644e6
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:41:53 +0600

    Initial commit

---
+++ CodeChef/OCT21A/MEXOR - MEX-OR/51890920.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key(k)  : Number of items strictly smaller than k.
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=1e5+5;
+const int N=1e6+5;
+const int mod=1e9+7;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int i,n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        if(__builtin_popcount(n+1) == 1)cout << n+1 << '\n';
+        else
+        {
+            for(i=30;i>=0;i--)
+            {
+                if((n >> i) & 1)
+                {
+                    cout << (1 << i) << '\n';
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
