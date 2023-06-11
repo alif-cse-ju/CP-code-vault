253887ff62cf3127e3864dc90b23df3f45f47440
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 16:08:19 +0600

    Initial commit

---
+++ CodeChef/LETS2021/LETSC002 - Sweet Delicacy/46708214.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define PI acos(-1)
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=1e5+5;
+const int N=2e5+5;
+const int mod=1e9+7;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    cout << __builtin_popcount(n);
+    return 0;
+}
