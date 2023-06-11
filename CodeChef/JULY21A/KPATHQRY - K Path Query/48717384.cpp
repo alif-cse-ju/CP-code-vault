fa7d0ed8bd62de843cc7f7b67dffc0b4c51d6dde
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:58:05 +0600

    Initial commit

---
+++ CodeChef/JULY21A/KPATHQRY - K Path Query/48717384.cpp
@@ -0,0 +1,128 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key(k)  : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=1e5+5;
+const int N=2e5+5;
+const int mod=1e9+7;
+vector<int>adj[N];
+int n,LG,timer,dist[N],dp[N][20],inTime[N],outTime[N];
+void DFS(int cur,int par)
+{
+    inTime[cur]=++timer;
+    dist[cur]=dist[par]+1;
+    dp[cur][0]=par;
+    for(int i=1; i<=LG; i++)dp[cur][i]=dp[dp[cur][i-1]][i-1];
+    for(int x : adj[cur])
+    {
+        if(x ^ par)DFS(x,cur);
+    }
+    outTime[cur]=++timer;
+}
+void PreCal()
+{
+    LG=ceil(log2(n));
+    dist[1]=timer=0;
+    DFS(1,1);
+}
+int FindKth(int node,int k)
+{
+    for(int i=LG; i>=0; i--)
+    {
+        if((1 << i) <= k)
+        {
+            k-=(1 << i);
+            node=dp[node][i];
+        }
+    }
+    return node;
+}
+bool IsAncestor(int x,int y)
+{
+    return inTime[x] <= inTime[y]  &&  outTime[x] >= outTime[y];
+}
+int LCA(int x,int y)
+{
+    if(IsAncestor(x,y))return x;
+    if(IsAncestor(y,x))return y;
+    for(int i=LG;i>=0;i--)
+    {
+        if(!IsAncestor(dp[x][i],y))x=dp[x][i];
+    }
+    return dp[x][0];
+}
+bool Compare(int &x,int &y)
+{
+    return dist[x] <= dist[y];
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    bool val;
+    int i,k,t,q,x,y;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        for(i=1; i<=n; i++)adj[i].clear();
+        for(i=1; i<n; i++)
+        {
+            cin >> x >> y;
+            adj[x].emplace_back(y);
+            adj[y].emplace_back(x);
+        }
+        PreCal();
+        cin >> q;
+        while(q--)
+        {
+            cin >> k;
+            vector<int>v1,v2;
+            for(i=0; i<k; i++)
+            {
+                cin >> x;
+                v1.emplace_back(x);
+            }
+            sort(v1.begin(),v1.end(),Compare);
+            for(i=0; i<k-1; i++)
+            {
+                if(FindKth(v1[k-1],dist[v1[k-1]]-dist[v1[i]]) != v1[i])v2.emplace_back(v1[i]);
+            }
+            k=(int)v2.size()-1;
+            for(i=0; i<k; i++)
+            {
+                if(FindKth(v2[k],dist[v2[k]]-dist[v2[i]]) != v2[i])break;
+            }
+            if(i >= k)
+            {
+                if(k < 0)cout << "YES\n";
+                else
+                {
+                    val=1;
+                    k=LCA(v1.back(),v2.back());
+                    for(int it : v1)
+                    {
+                        if(dist[it] < dist[k])
+                        {
+                            val=0;
+                            break;
+                        }
+                    }
+                    if(val)cout << "YES\n";
+                    else cout << "NO\n";
+                }
+            }
+            else cout << "NO\n";
+        }
+    }
+    return 0;
+}
