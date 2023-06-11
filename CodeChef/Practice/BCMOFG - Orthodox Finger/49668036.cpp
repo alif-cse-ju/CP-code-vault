caa292f5f0cbc767ac2adf1102f2daf8a23ea8a7
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 15:55:09 +0600

    Initial commit

---
+++ CodeChef/Practice/BCMOFG - Orthodox Finger/49668036.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int minOperation (int arr[], int n)
+{
+    unordered_map<int, int> hash;
+    for (int i=0; i<n; i++)
+        hash[arr[i]]++;
+    int max_count = 0;
+    for (auto i : hash)
+        if (max_count < i.second)
+            max_count = i.second;
+    return (n - max_count);
+}
+int main()
+{
+       int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+  cin>>arr[i];
+}
+    cout << minOperation(arr, n);
+    return 0;
+}
