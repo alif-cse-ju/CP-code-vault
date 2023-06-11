d25fea3b3eb5d019c914d8a3b763d9d29996e0da
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 16:23:32 +0600

    Initial commit

---
+++ CodeChef/Practice/UWCOI21A - Hidden Numbers/40565428.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=2e5+5;
+const int mod=1e9+7;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int n,t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        cout << "1 " << n << '\n';
+    }
+    return 0;
+}
