7ae5234aa10bb825d96211cae3aaac363e1021b1
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 16:09:38 +0600

    Initial commit

---
+++ CodeChef/START3A/TANDP - Thief and Police/45403264.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<typename temp>using ordered_set = tree<temp, null_type, less_equal<temp>, rb_tree_tag,tree_order_statistics_node_update
+//order_of_key (k) : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).//*(ost.find_by_order(k))
+#define PI acos(-1)
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL)
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]={-1,0,1,-1,1,-1,0,1};
+//int dy[]={1,1,1,0,0,-1,-1,-1};
+const int M=1e5+5;
+const int N=2e5+5;
+const int mod=1e9+7;
+int main()
+{
+    FAST();
+    int a,b,m,n,t,x,y;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> m >> x >> y >> a >> b;
+        if(abs(n-x)+abs(m-y) <= max(abs(a-n),abs(b-m)))cout << "YES\n";
+        else cout << "NO\n";
+    }
+    return 0;
+}
