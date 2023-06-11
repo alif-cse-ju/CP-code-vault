9ada596ebb1207269a86e9f614071d1d1495faa7
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2023-06-11 17:28:53 +0600

    Initial commit

---
+++ CodeChef/COJK2020/CKOJ20A - Zaikia and sticks/35836949.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define G getline
+#define SZ size()
+#define C clear()
+#define B begin()
+#define F front()
+#define T top()
+#define E end()
+#define EM empty()
+#define V vector
+#define Q queue
+#define DQ deque
+#define PQ priority_queue
+#define ST stack
+#define FI first
+#define SE second
+#define PI acos(-1)
+#define PS push
+#define PP pop()
+#define PSF push_front
+#define PSB push_back
+#define PPF pop_front()
+#define PPB pop_back()
+#define LL long long
+#define ULL unsigned long long
+#define PII pair<int,int>
+#define PSI pair<string,int>
+#define PIS pair<int,string>
+#define PLI pair<LL,int>
+#define PLL pair<LL,LL>
+#define MII map<int,int>
+#define MSI map<string,int>
+#define MIS map<int,string>
+#define MLI map<LL,int>
+#define FAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//int dx[]={-1,1,0,0};
+//int dy[]={0,0,-1,1};
+//int dx[]= {-1,0,1,-1,1,-1,0,1};
+//int dy[]= {1,1,1,0,0,-1,-1,-1};
+int main()
+{
+    FAST();
+    int i,n,a[200002];
+    cin >> n;
+    for(i=1;i<=n;i++)cin >> a[i];
+    sort(a+1,a+n+1);
+    for(i=n;i>2;i--)
+    {
+        if(a[i-1]+a[i-2] > a[i])
+        {
+            cout << "YES\n" << a[i] << ' ' << a[i-1] << ' ' << a[i-2];
+            return 0;
+        }
+    }
+    cout << "NO";
+    return 0;
+}
